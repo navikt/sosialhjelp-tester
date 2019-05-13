352709f05af7e011f539b61943e9082f75e60b74
Jan Tore Bøe <jan.tore.boe@nav.no>
2019-05-13 09:44:02 +0200

    Initial tests

--- src/minimal/minimal.ts
+++ src/minimal/minimal.ts
@@ -1,8 +1,24 @@
 import { Selector } from 'testcafe';
 
-fixture `Getting Started`
-    .page `https://www.google.com`;
+fixture `Minimal`
+    .page `https://sosialhjelp-test.herokuapp.com/soknadsosialhjelp/mock`;
 
 test('Grønt løp', async t => {
     await t
+        .typeText('#root > span > div > div > input', "tilfeldig")
+        .click('#login-button')
+        .click('#root > span > div > div > div:nth-child(10) > button')
+        .click("#start_soknad_button")
+        .click('#oppholdsadresse_folkeregistrert')
+        .click('#gaa_videre_button')
+        .click('#gaa_videre_button')
+        .click('#gaa_videre_button')
+        .click('#gaa_videre_button')
+        .click('#gaa_videre_button')
+        .click('#gaa_videre_button')
+        .click('#gaa_videre_button')
+        .click('#gaa_videre_button')
+        .click('#bekreft_oppsummering_checkbox')
+        .click('#gaa_videre_button')
+        .expect(Selector('#root > span > div > div.blokk-center.panel.ettersendelse__panel > div:nth-child(2) > div.avsnitt > h3 > span').innerText).eql('Søknaden er sendt til');
         });
--- src/minimal/minimal2.ts
+++
@@ -1,8 +0,0 @@
-import { Selector } from 'testcafe';
-
-fixture `Getting Started`
-    .page `https://www.google.com`;
-
-test('Grønt løp2', async t => {
-    await t
-        });
