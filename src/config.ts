64b7dc16426cd27886b817ce952f4aa3e7d00f6b
Eivind K. Nilsbakken <eivind.nilsbakken@nav.no>
2019-08-30 14:04:19 +0200

    Legg til test av søknadsStatus

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,22 @@
 export const config = {
-  "soknadUrl": "https://www.digisos-test.com/sosialhjelp/soknad",
-  "innsynUrl": "https://www.digisos-test.com/sosialhjelp/innsyn"
+    "soknadUrl": "https://www.digisos-test.com/sosialhjelp/soknad",
+    "innsynUrl": "https://www.digisos-test.com/sosialhjelp/innsyn",
+    "testData": {
+        "innsyn": {
+            "soknadsStatusMottatt": {
+                "avsender": {
+                    "systemnavn": "test",
+                    "systemversjon": "test"
+                },
+                "hendelser": [
+                    {
+                        "hendelsestidspunkt": "2019-01-01T12:00:00.000Z",
+                        "type": "soknadsStatus",
+                        "status": "MOTTATT"
+                    }
+                ],
+                "version": "test"
+            }
+        }
+    }
 };
