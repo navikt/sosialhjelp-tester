95df8c266b48fe5006a765c761e39b1e3493ea38
Eivind K. Nilsbakken <eivind.nilsbakken@nav.no>
2019-08-30 14:04:19 +0200

    Legg til test av søknadsStatus

--- src/innsyn/minimal/minimal.ts
+++ src/innsyn/minimal/minimal.ts
@@ -4,9 +4,18 @@ import { config } from '../../config';
 fixture `Minimal`
     .page(`${config.innsynUrl}/`);
 
-// TODO: Sjekk status på sak
+// TODO: Tilbakestill status etter test?
 test('Grønt løp', async t => {
     await t
-        .expect(Selector('.typo-sidetittel').innerText)
-        .eql('Sosialhjelp innsyn');
+        .click('html body div#pagewrapper.pagewrapper main#maincontent.maincontent div.maincontent.side-innhold div#root div.informasjon-side div.blokk-center div.panel ol.typo-normal li a')
+        .click('#operations-tag-mock-controller > a:nth-child(1) > span:nth-child(1)')
+        .click('.opblock-summary-post > span:nth-child(1)')
+        .click('.btn')
+        .selectText('.body-param__text', 0)
+        .typeText('.body-param__text', JSON.stringify(config.testData.innsyn.soknadsStatusMottatt))
+        .typeText('tr.parameters:nth-child(2) > td:nth-child(2) > input:nth-child(2)', '1234')
+        .click('.execute')
+
+        .navigateTo(`${config.innsynUrl}/status`)
+        .expect(Selector('.typo-innholdstittel').innerText).eql('Søknaden er mottatt');
 });
