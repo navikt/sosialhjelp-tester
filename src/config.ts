3c2b0e5b1442368e4120735d855d5e74c8b95986
Eivind K. Nilsbakken <eivind.nilsbakken@nav.no>
2019-09-05 08:15:57 +0200

    Fjern setting av hendelser med swagger

--- src/config.ts
+++ src/config.ts
@@ -1,22 +1,4 @@
 export const config = {
     "soknadUrl": "https://www.digisos-test.com/sosialhjelp/soknad",
-    "innsynUrl": "https://www.digisos-test.com/sosialhjelp/innsyn",
-    "testData": {
-        "innsyn": {
-            "soknadsStatusMottatt": {
-                "avsender": {
-                    "systemnavn": "test",
-                    "systemversjon": "test"
-                },
-                "hendelser": [
-                    {
-                        "hendelsestidspunkt": "2019-01-01T12:00:00.000Z",
-                        "type": "soknadsStatus",
-                        "status": "MOTTATT"
-                    }
-                ],
-                "version": "test"
-            }
-        }
-    }
+    "innsynUrl": "https://www.digisos-test.com/sosialhjelp/innsyn"
 };
--- src/innsyn/minimal/minimal.ts
+++ src/innsyn/minimal/minimal.ts
@@ -4,20 +4,10 @@ import { config } from '../../config';
 const fiksDigisosId = (new Date()).getTime().toString();
 
 fixture `Minimal`
-    .page(`${config.innsynUrl}/`);
+    .page(`${config.innsynUrl}/${fiksDigisosId}/status`);
 
 // TODO: Bruk fagsystem-mock til å sette innsyn-data
 test('Grønt løp', async t => {
     await t
-        .click('html body div#pagewrapper.pagewrapper main#maincontent.maincontent div.maincontent.side-innhold div#root div.informasjon-side div.blokk-center div.panel ol.typo-normal li a')
-        .click('#operations-tag-mock-controller > a:nth-child(1) > span:nth-child(1)')
-        .click('.opblock-summary-post > span:nth-child(1)')
-        .click('.btn')
-        .selectText('.body-param__text', 0)
-        .typeText('.body-param__text', JSON.stringify(config.testData.innsyn.soknadsStatusMottatt))
-        .typeText('tr.parameters:nth-child(2) > td:nth-child(2) > input:nth-child(2)', fiksDigisosId)
-        .click('.execute')
-
-        .navigateTo(`${config.innsynUrl}/${fiksDigisosId}/status`)
-        .expect(Selector('.typo-innholdstittel').innerText).eql('Søknaden er mottatt');
+        .expect(Selector('.typo-innholdstittel').innerText).eql('Søknaden er sendt');
 });
